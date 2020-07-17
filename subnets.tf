95754f26a9d7ce616d02123b18c9d99be8918b91
Adam Beamer <abeamer@amazon.com>
2020-07-17 19:46:00 +0000

    Fix subnets, add in .gitignore

--- subnets.tf
+++ subnets.tf
@@ -1,19 +1,23 @@
 resource "aws_subnet" "sgw-sub-a" {
+    vpc_id = aws_vpc.lxd.id
     cidr_block = var.sgw-sub-a-cidr
     availability_zone = "us-west-2a"
 }
 
 resource "aws_subnet" "lxd-sub-a" {
+    vpc_id = aws_vpc.lxd.id
     cidr_block = var.lxd-sub-a-cidr
     availability_zone = "us-west-2a"
 }
 
 resource "aws_subnet" "lxd-sub-b" {
+    vpc_id = aws_vpc.lxd.id
     cidr_block = var.lxd-sub-b-cidr
     availability_zone = "us-west-2b"
 }
 
-resouce "aws_subnet" "lxd-sub-c" {
+resource "aws_subnet" "lxd-sub-c" {
+    vpc_id = aws_vpc.lxd.id
     cidr_block = var.lxd-sub-c-cidr
     availability_zone = "us-west-2c"
 }
