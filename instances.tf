d67b94d1a381b36bae1bcd5ac8bb512a6e7eed67
abeamer <abeamer@amazon.com>
2020-07-18 16:01:33 -0600

    Fix variable names, change availability_zone to string

--- instances.tf
+++ instances.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "sgw1" {
     subnet_id = aws_subnet.sgw-sub-a.id
     association_public_ip = True
     vpc_security_group_ids = [ aws_security_group.sgw.id ]
-    availability_zone = us-west-2a
+    availability_zone = "us-west-2a"
     tags = {
         Name = "sgw1"
         Manager = "Terraform"
@@ -19,7 +19,7 @@ resource "aws_instance" "lxd-1" {
     instance_type = "t3a.small"
     subnet_id = aws_subnet.lxd-sub-a.id
     vpc_security_group_ids = [ aws_security_group.lxd.id ]
-    availability_zone = us-west-2a
+    availability_zone = "us-west-2a"
     tags = {
         Name = "lxd-1"
         Manager = "Terraform"
--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "lxd-sub-c-cidr" {
     default = "10.0.3.0/28"
 }
 
-variable "us-west-a-amd64-2004" {
+variable "us-west-2a-amd64-2004" {
     default = "ami-0bf357e4bc8a8b9e3"
 }
 variable "us-west-2a-arm64-2004" {
