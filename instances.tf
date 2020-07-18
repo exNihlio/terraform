e690079ec4f3cdb53d9138bdd6198209714c10cd
abeamer <abeamer@amazon.com>
2020-07-18 17:05:49 -0600

    Allow lxd-1 instance to use cloud-init

--- instances.tf
+++ instances.tf
@@ -17,6 +17,7 @@ resource "aws_instance" "sgw1" {
 resource "aws_instance" "lxd-1" {
     ami = var.us-west-2a-amd64-2004
     instance_type = "t3a.small"
+    user_data = file("cloud-init/base.yml")
     subnet_id = aws_subnet.lxd-sub-a.id
     vpc_security_group_ids = [ aws_security_group.lxd.id ]
     availability_zone = "us-west-2a"
