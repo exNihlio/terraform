c37bf60dfc33ca83f4d55794e9941b6050410ab2
abeamer <abeamer@amazon.com>
2020-07-18 17:13:19 -0600

    Tighten up the spacing in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,18 @@
 variable "vpc-cidr" {
     default = "10.0.0.0/16"
 }
-
 variable "sgw-sub-a-cidr" {
     default = "10.0.0.0/28"
 }
-
 variable "lxd-sub-a-cidr" {
     default = "10.0.1.0/28"
 }
-
 variable "lxd-sub-b-cidr" {
     default = "10.0.2.0/28"
 }
-
 variable "lxd-sub-c-cidr" {
     default = "10.0.3.0/28"
 }
-
 variable "us-west-2a-amd64-2004" {
     default = "ami-0bf357e4bc8a8b9e3"
 }
