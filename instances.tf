7ca39b7426740f063014752b4386ae44900691a0
abeamer <abeamer@amazon.com>
2020-07-18 16:05:25 -0600

    Correct typos in instances.tf

--- instances.tf
+++ instances.tf
@@ -2,9 +2,9 @@
 resource "aws_instance" "sgw1" {
     ami = var.us-west-2a-amd64-2004
     instance_type = "t3a.micro"
-    user_date = file("cloud-init/base.yml")
+    user_data = file("cloud-init/base.yml")
     subnet_id = aws_subnet.sgw-sub-a.id
-    association_public_ip = True
+    associate_public_ip = True
     vpc_security_group_ids = [ aws_security_group.sgw.id ]
     availability_zone = "us-west-2a"
     tags = {
