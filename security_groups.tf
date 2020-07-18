e11eddf2f8ec9f97a69aa45ae7102d6931fd44e6
abeamer <abeamer@amazon.com>
2020-07-18 16:26:51 -0600

    Update security groups for egress

--- security_groups.tf
+++ security_groups.tf
@@ -8,6 +8,12 @@ resource "aws_security_group" "sgw" {
         protocol = "TCP"
         cidr_blocks = [ "0.0.0.0/0" ]
     }
+    egress {
+        from_port = 0
+        to_port 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0"]
+    }
 }
 
 resource "aws_security_group" "lxd" {
@@ -32,5 +38,10 @@ resource "aws_security_group" "lxd" {
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
+    egress {
+        from_port = 0
+        to_port 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0"]
+    }
 }
