ecf4edf71545861ccde46f5e78690a36c443d4a6
abeamer <abeamer@amazon.com>
2020-07-18 15:44:34 -0600

    Add in instances

--- instances.tf
+++ instances.tf
@@ -1,3 +1,28 @@
-#resource "aws_instance" "lxd-1" {
-#
-#}
+# All AMIs are Ubuntu
+resource "aws_instance" "sgw1" {
+    ami = var.amis.us-west-2-amd64-2004
+    instance_type = "t3a.micro"
+    user_date = file("cloud-init/base.yml")
+    subnet_id = aws_subnet.sgw-sub-a.id
+    association_public_ip = True
+    vpc_security_group_ids = [ aws_security_group.sgw.id ]
+    availability_zone = us-west-2a
+    tags = {
+        Name = "sgw1"
+        Manager = "Terraform"
+        Role = "Security Gateway"
+    }
+}
+
+resource "aws_instance" "lxd-1" {
+    ami = var.amis.us-west-2-amd64-2004
+    instance_type = "t3a.small"
+    subnet_id = aws_subnet.lxd-sub-a.id
+    vpc_security_group_ids = [ aws_security_group.lxd.id ]
+    availability_zone = us-west-2a
+    tags = {
+        Name = "lxd-1"
+        Manager = "Terraform"
+        Role = "Container Host"
+    }
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,10 @@ variable "lxd-sub-b-cidr" {
 variable "lxd-sub-c-cidr" {
     default = "10.0.3.0/28"
 }
+
+variable "amis" {
+    # us-west-2 amd64 ubuntu 20.04
+    default = "ami-0bf357e4bc8a8b9e3"
+    us-west-2-amd64-2004 = "ami-0bf357e4bc8a8b9e3"
+    us-west-2-arm64-2004 = "ami-0c336ce0fc42f4e97"
+}
