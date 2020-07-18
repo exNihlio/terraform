8d2158a934bb60a9f44e2e4ae4cb2fec751209c7
abeamer <abeamer@amazon.com>
2020-07-18 16:28:06 -0600

    Fix port mappings

--- security_groups.tf
+++ security_groups.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "sgw" {
     }
     egress {
         from_port = 0
-        to_port 0
+        to_port = 0
         protocol = "-1"
         cidr_blocks = [ "0.0.0.0/0"]
     }
@@ -40,7 +40,7 @@ resource "aws_security_group" "lxd" {
     }
     egress {
         from_port = 0
-        to_port 0
+        to_port = 0
         protocol = "-1"
         cidr_blocks = [ "0.0.0.0/0"]
     }
