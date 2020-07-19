a8901484b7943244a78e512814ed5129f6cd7bf5
abeamer <abeamer@amazon.com>
2020-07-19 14:07:56 -0600

    Ensure resource has a name

--- attachments.tf
+++ attachments.tf
@@ -1,4 +1,4 @@
-resource "aws_volume_attachment" {
+resource "aws_volume_attachment""lxd-1-be" {
     device_name = "/dev/sdb"
     volume_id = aws_ebs_volume.lxd-1-be.id
     instance_id = aws_instance.lxd-1.id
