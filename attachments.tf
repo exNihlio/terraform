f4c75c568e57ef4dad39c6ea19637789e9448ecc
abeamer <abeamer@amazon.com>
2020-07-19 14:11:33 -0600

    Correct EBS device name

--- attachments.tf
+++ attachments.tf
@@ -1,5 +1,5 @@
 resource "aws_volume_attachment""lxd-1-be" {
-    device_name = "/dev/sdb"
+    device_name = "/dev/nvme1n1"
     volume_id = aws_ebs_volume.lxd-1-be.id
     instance_id = aws_instance.lxd-1.id
 }
