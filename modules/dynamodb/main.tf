d077bcf5b93c72d51cca6783bd9ca69c3e557938
abeamer <abeamer@amazon.com>
2020-07-23 18:28:44 -0600

    Further correct variable names

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,13 +1,13 @@
 resource "aws_dynamodb_table" "dynamodb-table" {
     name           = var.dynamodb_table_name
     billing_mode   = "PROVISIONED"
-    read_capacity  = var.rcu
-    write_capacity = var.wcu
+    read_capacity  = var.read_capacity
+    write_capacity = var.write_capacity
     hash_key       = var.hash_key
     range_key      = var.range_key
     # Iterate through DynamoDB attributes variable
     dynamic "attribute" {
-        for_each = [ for a in var.attributes: {
+        for_each = [ for a in var.dynamodb_attributes: {
             name = a.name
             type = a.type
 
