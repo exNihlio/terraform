326f41cc0de898f06d5d9dc3ec1dabc225a4525d
abeamer <abeamer@amazon.com>
2020-07-23 18:02:53 -0600

    Fix attribute iteration

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,15 +1,20 @@
 resource "aws_dynamodb_table" "dynamodb-table" {
-  name           = var.dynamodb_table_name
-  billing_mode   = "PROVISIONED"
-  read_capacity  = var.rcu
-  write_capacity = var.wcu
-  hash_key       = var.hash_key
-  range_key      = var.range_key
-  # Iterate through DynamoDB attributes variable
-  dynamic "attribute" {
-    for_each = [ for a in var.attributes ]:
-    content {
-        name = a.name
-        type = a.type
+    name           = var.dynamodb_table_name
+    billing_mode   = "PROVISIONED"
+    read_capacity  = var.rcu
+    write_capacity = var.wcu
+    hash_key       = var.hash_key
+    range_key      = var.range_key
+    # Iterate through DynamoDB attributes variable
+    dynamic "attribute" {
+        for_each = [ for a in var.attributes: {
+            name = a.name
+            type = a.type
+
+        }]
+        content {
+            name = attribute.value.name
+            type = attribute.value.type
+        }
     }
 }
