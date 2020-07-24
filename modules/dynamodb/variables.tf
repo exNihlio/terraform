ad030202571eafec4469864645a1e7e37fa01495
abeamer <abeamer@amazon.com>
2020-07-23 18:25:35 -0600

    Update variables

--- modules/dynamodb/variables.tf
+++ modules/dynamodb/variables.tf
@@ -1,3 +1,23 @@
 variable "dynamodb_attribtues" {
     default = []
 }
+
+variable "read_capacity" {
+    default = []
+}
+
+variable "write_capacity" {
+    default = []
+}
+
+variable "range_key" {
+    default = []
+}
+
+variable "hash_key" {
+    default = []
+}
+
+variable "name" {
+    default = []
+}
