546898323cd3f5199fec956b58eaf6156b67bd13
abeamer <abeamer@amazon.com>
2020-07-28 00:28:55 +0000

    003-vpc-with-igw example now validates

--- examples/aws/vpc/003-vpc-with-igw/subnets.tf
+++ examples/aws/vpc/003-vpc-with-igw/subnets.tf
@@ -1,11 +1,11 @@
 resource "aws_subnet" "pub_sub_1" {
     cidr_block = "10.72.1.0/24"
-    vpc_id = aws_vpc.terraform-example.id
+    vpc_id = aws_vpc.terraform-example-vpc.id
     availability_zone = "us-west-2a"
 }
 
 resource "aws_subnet" "priv_sub_1" {
     cidr_block = "10.72.2.0/24"
-    vpc_id = aws_vpc.terraform-example.id
+    vpc_id = aws_vpc.terraform-example-vpc.id
     availability_zone = "us-west-2a"
 }
