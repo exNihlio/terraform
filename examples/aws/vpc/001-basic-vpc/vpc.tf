8b48ee54c78c3ca81500229d17f030e928a3cb82
abeamer <abeamer@amazon.com>
2020-07-28 00:18:22 +0000

    Example 001-basic-vpc validate

--- examples/aws/vpc/001-basic-vpc/vpc.tf
+++ examples/aws/vpc/001-basic-vpc/vpc.tf
@@ -1,6 +1,6 @@
-resource "aws_vpc" "terraform-example" {
+resource "aws_vpc" "terraform-example-vpc" {
     # cidr_block is the only required argument for the VPC resource
-    cidr_block = "10.72.72.0/16"
+    cidr_block = "10.72.0.0/16"
     # Virtually every AWS provider resource supports the tag block.
     # It is recommended you use it, at the least to give things
     # friendly names and declare that it is managed by Terraform
