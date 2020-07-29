a11b82e1f1fdc4ef4700bf9ba38917659e80a6f8
exNihlio <adam.k.beamer+github@gmail.com>
2020-07-29 09:20:59 -0600

    Correct type in jsondecode

--- examples/aws/modules/dynamodb/main.tf
+++ examples/aws/modules/dynamodb/main.tf
@@ -25,5 +25,5 @@ module "dynamdb_table_one" {
     hash_key = var.hash_key
     attributes = var.attributes
     # Read in data here
-    table_item = jsondecod(file("data/data.json"))
+    table_item = jsondecode(file("data/data.json"))
 }
