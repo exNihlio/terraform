bca9bd670f3fec72d9cc8c61bf7ec951fd6494c4
exNihlio <adam.k.beamer+github@gmail.com>
2020-08-14 15:57:43 -0600

    Comment substantially and add a README.md to examples/aws/CodeBuild

--- examples/aws/CodeBuild/policies.tf
+++ examples/aws/CodeBuild/policies.tf
@@ -1,3 +1,5 @@
+# Attach the policy allowing to ECR write/query/pull privileges
+# and CloudWatch logs privileges to CodeBuild
 resource "aws_iam_policy" "CodeBuild_ecr" {
     name = "CodeBuild_ecr"
     policy = file("policies/CodeBuild_policy.json")
