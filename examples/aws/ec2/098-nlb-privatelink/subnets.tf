107d28501e9d5a513b743f59ebb627ba26de4899
exNihlio <Adam.K.Beamer+github@gmail.com>
2020-09-16 16:10:44 +0000

    Subnet chnages

--- examples/aws/ec2/098-nlb-privatelink/subnets.tf
+++ examples/aws/ec2/098-nlb-privatelink/subnets.tf
@@ -1,23 +1,71 @@
-resource "aws_subnet" "pub-sub-1" {
+####################
+## Client Subnets ##
+####################
+
+resource "aws_subnet" "pub_sub_1" {
     cidr_block = "10.72.1.0/24"
-    vpc_id = aws_vpc.privatelink-vpc.id
+    vpc_id = aws_vpc.privatelink_client_vpc.id
     availability_zone = "us-west-2a"
+    tags = {
+        Name = "privatelink-pub-sub-1"
+        Manager = "terraform"
+    }
 }
 
-resource "aws_subnet" "priv-sub-1" {
+# Private subnet
+resource "aws_subnet" "priv_sub_1" {
     cidr_block = "10.72.2.0/24"
-    vpc_id = aws_vpc.privatelink-vpc.id
+    vpc_id = aws_vpc.privatelink_client_vpc.id
     availability_zone = "us-west-2a"
+    tags = {
+        Name = "privatelink-priv-sub-1"
+        Manager = "terraform"
+    }
 }
 
-resource "aws_subnet" "priv-sub-2" {
-    cidr_block = "10.72.3.0/24"
-    vpc_id = aws_vpc.privatelink-vpc.id
-    availability_zone = "us-west-2b"
+## PrivateLink Endpoint
+resource "aws_subnet" "priv_endpoint_1" {
+    cidr_block = "10.72.5.0/24"
+    vpc_id = aws_vpc.privatelink_client_vpc.id
+    availability_zone = "us-west-2a"
+    tags = {
+        Name = "privatelink-endpoint-sub-1"
+        Manager = "terraform"
+    }
 }
 
-resource "aws_subnet" "priv-sub-3" {
-    cidr_block = "10.72.4.0/24"
-    vpc_id = aws_vpc.privatelink-vpc.id
-    availability_zone = "us-west-2c"
+########################
+## Server VPC Subnets ##
+########################
+
+resource "aws_subnet" "server_pub_sub_1" {
+    cidr_block = "10.73.1.0/24"
+    vpc_id = aws_vpc.privatelink_server_vpc.id
+    availability_zone = "us-west-2a"
+    tags = {
+        Name = "server-pub-sub-1"
+        Manager = "terraform"
+    }
+}
+
+# Private subnets
+resource "aws_subnet" "server_priv_sub_1" {
+    cidr_block = "10.73.2.0/24"
+    vpc_id = aws_vpc.privatelink_server_vpc.id
+    availability_zone = "us-west-2a"
+    tags = {
+        Name = "server-priv-sub-1"
+        Manager = "terraform"
+    }
+}
+
+## PrivateLink Endpoint
+resource "aws_subnet" "priv_endpoint_2" {
+    cidr_block = "10.73.5.0/24"
+    vpc_id = aws_vpc.privatelink_server_vpc.id
+    availability_zone = "us-west-2a"
+    tags = {
+        Name = "server-endpoint-sub-1"
+        Manager = "terraform"
+    }
 }
