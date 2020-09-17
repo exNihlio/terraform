1f7d574d5d5cfc42b862a59188431bc8e8bd689e
exNihlio <Adam.K.Beamer+github@gmail.com>
2020-09-17 06:32:18 +0000

    Don't try to create the endpoint service until the NLB is created

--- examples/aws/ec2/098-nlb-privatelink/vpc_endpoint_service.tf
+++ examples/aws/ec2/098-nlb-privatelink/vpc_endpoint_service.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc_endpoint_service" "privatelink_service" {
     acceptance_required = false
     network_load_balancer_arns = [aws_lb.nlb_private_link.arn]
+    depends_on = [aws_lb.nlb_private_link]
 }
