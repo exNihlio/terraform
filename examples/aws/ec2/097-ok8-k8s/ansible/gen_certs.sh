a0ff553b39ebbf57607cd5882b31b1fd5af80829
exNihlio <Adam.K.Beamer+github@gmail.com>
2020-10-01 14:44:38 +0000

    Update outputs to include all IP addresses from worker nodes and etcd nodes. Generate certs with cfssl

--- examples/aws/ec2/097-ok8-k8s/ansible/gen_certs.sh
+++ examples/aws/ec2/097-ok8-k8s/ansible/gen_certs.sh
@@ -3,16 +3,26 @@
 export ASCII_RED="\u001b[31;1m"
 export ASCII_RESET="\u001b[0m"
 
-## Etcd IP addresses
+#source vars.sh
+
+declare -A ETCD_ADDRESSES
+declare -A WORKER_ADDRESSES
+
 export ETCD_HOST_1_IP="10.72.5.46"
 export ETCD_HOST_2_IP="10.72.6.153"
 export ETCD_HOST_3_IP="10.72.7.41"
 
-declare -A IP_ADDRESSES
+export WORKER_HOST_1_IP="10.72.5.46"
+export WORKER_HOST_2_IP="10.72.6.153"
+export WORKER_HOST_3_IP="10.72.7.41"
 
-IP_ADDRESSES=(["etcd-node-1.okd.dev"]=${ETCD_HOST_1_IP}
-              ["etcd-node-2.okd.dev"]=${ETCD_HOST_2_IP}
-              ["etcd-node-3.okd.dev"]=${ETCD_HOST_3_IP})
+ETCD_ADDRESSES=(["etcd-node-1.okd.dev"]=${ETCD_HOST_1_IP}
+                ["etcd-node-2.okd.dev"]=${ETCD_HOST_2_IP}
+                ["etcd-node-3.okd.dev"]=${ETCD_HOST_3_IP})
+
+WORKER_ADDRESSES=(["okd-node-1.okd.dev"]=${WORKER_HOST_1_IP}
+                  ["okd-node-2.okd.dev"]=${WORKER_HOST_2_IP}
+                  ["okd-node-3.okd.dev"]=${WORKER_HOST_3_IP})
 
 mkdir -p certs
 
@@ -33,11 +43,12 @@ cd certs \
    ../files/admin-csr.json 2>/dev/null | cfssljson -bare admin \
 && cd ..
 
+## Generate the etcd certs
 echo -e "${ASCII_RED}Generating etcd certs${ASCII_RESET}"
 cd certs
 for i in $(seq 1 3 ); do
-  echo -e "${ASCII_RED}Generating etcd-node-${i} certs${ASCII_RESET}"
-  ADDRESS=$(echo ${IP_ADDRESSES[etcd-node-${i}.okd.dev]})
+  echo -e "${ASCII_RED}Generating etcd-node-${i} cert${ASCII_RESET}"
+  ADDRESS=$(echo ${ETCD_ADDRESSES[etcd-node-${i}.okd.dev]})
   cfssl gencert 2>/dev/null \
   -ca=ca.pem \
   -ca-key=ca-key.pem \
@@ -45,5 +56,21 @@ for i in $(seq 1 3 ); do
   -hostname=etcd-node-${i}.okd.dev,${ADDRESS} \
   -profile=kubernetes \
   ../files/etcd-node-${i}-csr.json | cfssljson -bare etcd-node-${i}.okd.dev
+done
+cd ..
+
+# Generate the worker node certs
+echo -e "${ASCII_RED}Generating okd certs${ASCII_RESET}"
+cd certs
+for i in $(seq 1 3 ); do
+  echo -e "${ASCII_RED}Generating okd-node-${i} cert${ASCII_RESET}"
+  ADDRESS=$(echo ${WORKER_ADDRESSES[okd-node-${i}.okd.dev]})
+  cfssl gencert 2>/dev/null \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=../files/ca-config.json \
+  -hostname=okd-node-${i}.okd.dev,${ADDRESS} \
+  -profile=kubernetes \
+  ../files/okd-node-${i}-csr.json | cfssljson -bare okd-node-${i}.okd.dev
 done
 cd ..
--- examples/aws/ec2/097-ok8-k8s/outputs.tf
+++ examples/aws/ec2/097-ok8-k8s/outputs.tf
@@ -13,3 +13,15 @@ output "ETCD_HOST_2" {
 output "ETCD_HOST_3" {
     value = aws_instance.etcd-node-3.private_ip
 }
+
+output "WORKER_HOST_1" {
+    value = aws_instance.okd-node-1.private_ip
+}
+
+output "WORKER_HOST_2" {
+    value = aws_instance.okd-node-2.private_ip
+}
+
+output "WORKER_HOST_3" {
+    value = aws_instance.okd-node-3.private_ip
+}
