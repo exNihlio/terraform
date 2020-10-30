c44e01b70f93c6ca198fe92f6aa6f5ff10a3a609
exNihlio <exNihlio@sami.dog>
2020-10-30 20:37:04 +0000

    Add in initial ansible configuration

--- examples/aws/ec2/097-ok8-k8s/variables.tf
+++ examples/aws/ec2/097-ok8-k8s/variables.tf
@@ -5,3 +5,15 @@ variable "us-west-2a-centos8-amd64" {
 variable "us-west-2a-centos7-amd64" {
     default = "ami-0a248ce88bcc7bd23"
 }
+
+variable "us-west-2a-centos7-amd64" {
+    default = "ami-0a248ce88bcc7bd23"
+}
+
+variable "us-west-2a-ubuntu1804-amd64" {
+    default = "ami-0a248ce88bcc7bd23"
+}
+
+variable "us-west-2a-ubuntu2004-amd64" {
+    default = "ami-0a248ce88bcc7bd23"
+}
