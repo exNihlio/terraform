473a5b4cade55b62db2eddc6d8696228854ccf39
exNihlio <Adam.K.Beamer+github@gmail.com>
2021-02-21 21:44:02 +0000

    Deploy service in two subnets

--- examples/aws/Fargate/001-intro/ecs_services.tf
+++ examples/aws/Fargate/001-intro/ecs_services.tf
@@ -6,19 +6,19 @@ resource "aws_ecs_service" "flaskapp_az1" {
     iam_role = aws_iam_role.ecsTaskExecutionRole.arn
     launch_type = "FARGATE"
     network_configuration {
-        subnets = [aws_subnet.sub_priv_1.id]
+        subnets = [aws_subnet.sub_priv_1.id,aws_subnet.sub_priv_2.id]
         security_groups = [aws_security_group.flaskapp_sg.id]
     }
 }
-resource "aws_ecs_service" "flaskapp_az2" {
-    name = "flaskapp_az2"
-    cluster = aws_ecs_cluster.flask_cluster.id
-    task_definition = aws_ecs_task_definition.flaskapp.arn
-    desired_count = 0
-    iam_role = aws_iam_role.ecsTaskExecutionRole.arn
-    launch_type = "FARGATE"
-    network_configuration {
-        subnets = [aws_subnet.sub_priv_2.id]
-        security_groups = [aws_security_group.flaskapp_sg.id]
-    }
-}
+# resource "aws_ecs_service" "flaskapp_az2" {
+#     name = "flaskapp_az2"
+#     cluster = aws_ecs_cluster.flask_cluster.id
+#     task_definition = aws_ecs_task_definition.flaskapp.arn
+#     desired_count = 0
+#     iam_role = aws_iam_role.ecsTaskExecutionRole.arn
+#     launch_type = "FARGATE"
+#     network_configuration {
+#         subnets = [aws_subnet.sub_priv_2.id]
+#         security_groups = [aws_security_group.flaskapp_sg.id]
+#     }
+# }
